46d2ceabb42dce8d8d1cfc9ab903879b9d0268f9
hager445 <ho2333535@gmail.com>
2026-03-08 16:25:33 +0200

    update ratiing number length

--- src/app/shared/components/ratingstars/ratingstars.component.ts
+++ src/app/shared/components/ratingstars/ratingstars.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { ArrayPipe } from '../../../core/pipes/array.pipe';
 import { NotintegerPipe } from '../../../core/pipes/notinteger.pipe';
+import { TrimPipe } from '../../../core/pipes/trim.pipe';
 
 @Component({
   selector: 'app-ratingstars',
-  imports: [ArrayPipe, NotintegerPipe],
+  imports: [ArrayPipe, NotintegerPipe, TrimPipe],
   templateUrl: './ratingstars.component.html',
-  styleUrl: './ratingstars.component.scss'
+  styleUrl: './ratingstars.component.scss',
 })
 export class RatingstarsComponent {
-@Input() ratingAverage!:number;
+  @Input() ratingAverage!: number;
 }
